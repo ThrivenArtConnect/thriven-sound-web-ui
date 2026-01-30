f6e2577ebe3cf6fab9aea57674a477925286c3ad
SiCKaRiM <thriven.art.essence@gmail.com>
2026-01-30 04:13:06 +0100

    Delete app directory

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: '2rem', fontFamily: 'system-ui' }}>
-      <h1>🎵 Thriven Sound Analyzer</h1>
-      <p>Web UI für Suno Stem Processing & BR-864 Export</p>
-    </main>
-  );
-}
